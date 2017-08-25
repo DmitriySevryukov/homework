76f991dcef49a8f69d6b09c0cdc9b0e90b4af786
Леня <@COMP>
2017-08-25 22:26:45 +0300

    bank

---
+++ Task5.8/Task5.8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter amount of money you want to put: ");
+            double m = double.Parse(Console.ReadLine());
+            double percent = m * 0.2;
+            Console.Write("Enter number of years: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                m += percent;
+            }
+            Console.Write("In {0} years ballance of your account will be {1}", n, m);
+
+            Console.ReadLine();
+        }
+    }
+}
