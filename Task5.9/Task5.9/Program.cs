48eca2f2f681635210e92b1246ba1d80bf918558
Леня <@COMP>
2017-08-25 22:45:22 +0300

    numbers

---
+++ Task5.9/Task5.9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numbers from 1 to 50 divisible by 3 but not divisible by 5 are following:");
+            for (int i = 1; i <= 50; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i % 5 != 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
